1975c32e4bc922f8abadda77e0890dabc4d17552
Houcem <houcem.laouiti@gmail.com>
2023-04-20 15:04:45 +0100

    minor bug fixes

--- assets/js/averos-scripts.js
+++ assets/js/averos-scripts.js
@@ -45,8 +45,13 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  if (slides.length>0){
+    slides[slideIndex-1].style.display = "block";
+  }
+  if (dots.length>0){
+    dots[slideIndex-1].className += " active";
+  }
+  
 }
 
 showSlidesAutomatically();
